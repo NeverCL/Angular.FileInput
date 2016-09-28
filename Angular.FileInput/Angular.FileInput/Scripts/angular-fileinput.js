27d7d23cc496395b07b0fe237a3e87320c6e1c81
Never、C <caolin>
2016-09-28 15:52:22 +0800

    add bs.fileinput

--- Angular.FileInput/Angular.FileInput/Scripts/angular-fileinput.js
+++ Angular.FileInput/Angular.FileInput/Scripts/angular-fileinput.js
@@ -1,1 +1,28 @@
-﻿
+﻿(function (root, factory) {
+    if (typeof root.define === 'function' && root.define.amd) {
+        root.define(['angular', 'fileinput'], factory);
+    } else factory();
+}(window, function () {
+    'use strict';
+    angular.module('bs.fileinput', []).directive('file', function () {
+        return {
+            require: 'ngModel',
+            restrict: 'C',
+            link: fileLink
+        };
+
+        function fileLink(scope, ele, attr, ngModel) {
+            if (attr.multiple) {
+                $(ele).fileinput({ uploadUrl: 'UploadFile.ashx', language: 'zh', uploadAsync: false });
+                $(ele).on('filebatchuploadsuccess', function (evt, data) {
+                    var rst = data.response; ngModel.$setViewValue(rst);
+                });
+            } else {
+                $(ele).fileinput({ uploadUrl: 'UploadFile.ashx', language: 'zh' });
+                $(ele).on('fileuploaded', function (evt, data) {
+                    var rst = data.response; ngModel.$setViewValue(rst[0]);
+                });
+            }
+        }
+    });
+}));
---
+++ Angular.FileInput/Angular.FileInput/UploadFile.ashx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular.FileInput
+{
+    /// <summary>
+    /// UploadFile 的摘要说明
+    /// </summary>
+    public class UploadFile : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            var ids = new List<long>();
+            for (int i = 0; i < context.Request.Files.Count; i++)
+            {
+                ids.Add(i + 1);
+            }
+            context.Response.Write('[' + string.Join(",", ids) + ']');
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
